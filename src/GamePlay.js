a8e2e967dbdd749fe282ad4682a02e56c603d7bf
ethanial1 <camiloeiz00@gmail.com>
2021-08-25 11:07:37 -0500

    Propiedades del caballo

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -11,13 +11,30 @@ GamePlayManager = {
     // se cargan todos los recursos que necesitamos para el proyecto, una vez cargados se llama al método create
     preload: function () {
         // cargamos el fondo con load.image(nombreDeInstancia, rutaDeImagen);
-        game.load.image('background', 'assets/images/background.png')
+        game.load.image('background', 'assets/images/background.png');
+
+        // Sprites (identificado, ruta, ancho, alto, númerDeImágenes)
+        game.load.spritesheet('horse', 'assets/images/horse.png', 84,156,2)
     },
 
     create: function () {
         //                                              En la esquina izquierda superior
         // ponemos la imagen en pantalla con add.sprite(coordenadas, coordenadas, imagen )
         game.add.sprite(0, 0,'background');
+
+        // Sprites
+        // añadimos propiedades
+        this.horse = game.add.sprite(0,0, 'horse');
+        this.horse.frame = 1; // ojo cerrado: 0, ojo abierto: 1, (Son solo dos frames)
+        // cambiamos la posición de x,y
+        this.horse.x = game.width / 2;
+        this.horse.x = game.height / 2;
+
+        // Cambiamos el anchor
+        this.horse.anchor.setTo(0.5,0.5); // x,y
+
+        // Rotar
+        this.horse.angle = 15; // en grados a partir de su anchor
     },
     // Phaser llama frame a frame al método update
     update: function () {
