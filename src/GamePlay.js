08bcc029aa2d6657a06f5261a161d461ebb14806
ethanial1 <camiloeiz00@gmail.com>
2021-08-25 09:46:48 -0500

    Objeto de phaser

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,6 +1,33 @@
+// Objeto
+GamePlayManager = {
+    // phaser llama a init una vez que declaremos el estado start
+    init: function() {
+        
+    },
+    // se cargan todos los recursos que necesitamos para el proyecto, una vez cargados se llama al método create
+    preload: function () {
+        
+    },
+
+    create: function () {
+
+    },
+    // Phaser llama frame a frame al método update
+    update: function () {
+        
+    }
+}
+
+
 // Variable que guarda la instancia de phase.
 // Game(width, height, render)
 // RENDERS:
 //  - Phase.WEBGL: Implementación gráfica que permite utilizar la tarjeta de video. (Render rápido)
 //  - Phase.CANVAS: Utilizado en caso de no tener una tarjeta de video en el dispositivo. (Render lento)
-var game = new Phaser.Game(1136, 640, )
+//  - Phase.AUTO: Indicamos a phase que intente utilizar WEBGL y en caso de no tenerlo, utilizar canvas.
+
+var game = new Phaser.Game(1136, 640, Phaser.AUTO);
+
+// agremamos un estado
+game.state.add('gameplay', GamePlayManager);
+game.state.start("gameplay")
